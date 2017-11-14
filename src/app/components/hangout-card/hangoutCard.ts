f09c692b015638106ae26f17395d052097deb185
Abhishek Shetty <abhishek10.rs@gmail.com>
2017-11-14 01:15:12 +0100

    made home page reactive

--- src/app/components/hangout-card/hangoutCard.ts
+++ src/app/components/hangout-card/hangoutCard.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Hangout } from '../../../pages/hangout/hangout';
+import { UserData } from '../../../assets/data/UserData';
 
 @Component({
   selector: 'hangout-card',
@@ -8,12 +9,22 @@ import { Hangout } from '../../../pages/hangout/hangout';
 })
 
 export class HangoutCard {
+@Input('hangout') data: any;
+userData = UserData;
+toTime: String;
+fromTime: String;
+  
   constructor(public navCtrl: NavController) {
 
   }
 
+  ngOnInit(){
+    this.fromTime = this.data.fromTime.toLocaleString();
+    this.toTime = this.data.toTime.toLocaleString();
+  }
+
   handleClick(){
-    this.navCtrl.push(Hangout); 
+    this.navCtrl.push(Hangout, this.data); 
   }
   
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Hangout } from '../hangout/hangout';
 import { HangoutCard } from '../../app/components/hangout-card/hangoutCard';
+import { HangoutData, Comments } from '../../assets/data/HangoutData';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  hangoutData = HangoutData;
   constructor(public navCtrl: NavController) {
 
   }
